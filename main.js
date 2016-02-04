ec4d666841ded90121248041006f04a6a2fb9488
Daan Beverdam <daanbeverdam@gmail.com>
2016-02-04 13:20:12 +0100

    circleRadii

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 console.log('I see you came to the console...');
 
-var array = [1, 2, 3];
+var circleRadii = [40, 20, 10];
 
-var p = d3.select('body')
-        .selectAll('p')
-        .data(array)
-        .enter()
-        .append('p')
-        .text(function (d) {
-            return d;
-        });
+var svgContainer = d3.select('body')
+                      .append('svg')
+                      .attr('width', 200)
+                      .attr('height', 200);
 
-console.log(p);
+var circles = svgContainer.selectAll('circle')
+                          .data(circleRadii)
+                          .enter()
+                          .append('circle');
 
-var s = d3.select("body")
-        .append("svg")
-        .attr("width", 50)
-        .attr("height", 50)
-        .append("circle")
-        .attr("cx", 25)
-        .attr("cy", 25)
-        .attr("r", 25)
-        .style("fill", "purple");
+var circleAttributes = circles
+                       .attr('cx', 50)
+                       .attr('cy', 50)
+                       .attr('r', function (d) {
+                           return d;
+                       })
+                       .style('fill', function (d) {
+                           var color;
+                           if (d === 40){
+                               color = 'green';
+                           } else if (d === 20){
+                               color = 'yellow';
+                           } else {
+                               color = 'blue';
+                           }
+                           return color;
+                       });
+
+console.log(circleAttributes);
+
+// var s = d3.select("body")
+//         .append("svg")
+//         .attr("width", 50)
+//         .attr("height", 50)
+//         .append("circle")
+//         .attr("cx", 25)
+//         .attr("cy", 25)
+//         .attr("r", 25)
+//         .style("fill", "purple");
